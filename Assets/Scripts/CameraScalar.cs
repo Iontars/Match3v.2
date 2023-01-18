8c8936ce9e9f630e1952e0b89410ef5c26ac001f
Граница миров <fmycry1@gmail.com>
2023-01-18 23:11:40 +0700

    1
    win 10

--- Assets/Scripts/CameraScalar.cs
+++ Assets/Scripts/CameraScalar.cs
@@ -7,18 +7,22 @@ public class CameraScalar : MonoBehaviour
     private Board board;
     public float cameraOffset;
     //public float aspectRatio = 0.5625f; // ширина разделённая на высоту разрешения экрана (1080/1920)
-    public float aspectRatio = .4615f; // ширина разделённая на высоту разрешения экрана (1080/2340)
+    float aspectRatio;  // ширина разделённая на высоту разрешения экрана (1080/2340)
     public float pudding = 10;
     public float yOffset = 1;
 
     void Start()
     {
+        aspectRatio = (float)Screen.width / Screen.height;
         //board = gameObject.AddComponent(typeof(Board)) as Board;
         board = FindObjectOfType<Board>();
         if (board != null)
         {
             RepositionCamera(board.width - 1, board.height - 1, cameraOffset);
         }
+        print(Screen.width);
+        print(Screen.height);
+        print(aspectRatio);
     }
 
     void RepositionCamera(float x, float y, float z)
@@ -27,15 +31,13 @@ public class CameraScalar : MonoBehaviour
         transform.position = tempPosition;
         if (board.width >= board.height)
         {
-            //Camera.main.orthographicSize = board.height / 2 + pudding;
-            Camera.main.orthographicSize = board.height + 1;
-            //Debug.LogWarning("ширина доски больше высоты");
+            Camera.main.orthographicSize = board.height / 2 + pudding;
+            Debug.LogWarning("ширина доски больше высоты");
         }
         else
         {
-            //Camera.main.orthographicSize = (board.width / 2 + pudding) / aspectRatio;
-            Camera.main.orthographicSize = board.width + 1;
-            //Debug.LogWarning("высота доски больше ширины");
+            Camera.main.orthographicSize = (board.width / 2 + pudding) / aspectRatio;
+            Debug.LogWarning("высота доски больше ширины");
         }
     }
 
