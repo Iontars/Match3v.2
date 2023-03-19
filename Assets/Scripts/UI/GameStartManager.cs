701a3b7056f0b33e9dda380a12b9eb36d6762b66
Граница миров <fmycry1@gmail.com>
2023-03-19 09:55:34 +0700

    1

---
+++ Assets/Scripts/UI/GameStartManager.cs
@@ -0,0 +1,44 @@
+#region Using
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using TMPro;
+#endregion
+
+/// <summary> Descriptions </summary>
+public class GameStartManager : MonoBehaviour
+{
+    public GameObject startPanel;
+    public GameObject levelPanel;
+    void Awake()
+    {
+        startPanel.SetActive(true);
+        levelPanel.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        startPanel.SetActive(false);
+        levelPanel.SetActive(true);
+    }
+
+    public void Home()
+    {
+        startPanel.SetActive(true);
+        levelPanel.SetActive(false);
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
