2d8aa4a724a90747b953349ed778d6b2d521bda4
Граница миров <fmycry1@gmail.com>
2023-04-14 12:35:33 +0700

    1

---
+++ Assets/Scripts/Adventure/Fields/FieldsBonusStorage.cs
@@ -0,0 +1,29 @@
+#region Using
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using TMPro;
+#endregion
+
+/// <summary> Descriptions </summary>
+public class FieldsBonusStorage : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
